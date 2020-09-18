974bb718be62a3219932451d62a1601a42f05b22
EpochCode <he.george10@gmail.com>
2020-09-18 18:41:45 -0400

    Updates

---
+++ precache-manifest.cd5580b10db7a14ecce90043ef22bd46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9236f34c6cf8edcb67570e5efe99a083",
+    "url": "/UWCC-react/index.html"
+  },
+  {
+    "revision": "cac389250d93ad7e306e",
+    "url": "/UWCC-react/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "ee64b5eedf7d3fe7cfe2",
+    "url": "/UWCC-react/static/css/main.f76d161d.chunk.css"
+  },
+  {
+    "revision": "cac389250d93ad7e306e",
+    "url": "/UWCC-react/static/js/2.cb77dd33.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/UWCC-react/static/js/2.cb77dd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee64b5eedf7d3fe7cfe2",
+    "url": "/UWCC-react/static/js/main.e47c6627.chunk.js"
+  },
+  {
+    "revision": "a9ef92095b2cf746a05b",
+    "url": "/UWCC-react/static/js/runtime-main.3fb556b0.js"
+  }
+]);
